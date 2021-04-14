b9f67fdd2caa6178ae65bfce62d190b3c71aad55
LucasLaughlin <lucas_laughlin@yahoo.com>
2021-04-13 20:17:15 -0600

    Added new Death graph

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
-import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
-import routes from 'src/routes';
+import DashboardLayout from 'src/layouts/DashboardLayout';
+import DashboardView from 'src/views/reports/DashboardView';
 
 const App = () => {
-  const routing = useRoutes(routes);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {routing}
+      <DashboardLayout>
+        <DashboardView />
+      </DashboardLayout>
     </ThemeProvider>
   );
 };
---
+++ src/services/AthenaExpress.service.js
@@ -0,0 +1,11 @@
+/* const aws = require('aws-sdk');
+
+const awsCredentials = {
+  region: 'YOUR_AWS_REGION',
+  accessKeyId: 'YOUR_AWS_ACCESS_KEY_ID',
+  secretAccessKey: 'YOUR_AWS_SECRET_ACCESS_KEY'
+};
+aws.config.update(awsCredentials);
+
+const athenaExpressConfig = { aws }; // configuring athena-express with aws sdk object
+const athenaExpress = new AthenaExpress(athenaExpressConfig); */
